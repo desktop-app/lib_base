cfc1a874eb603c345f60be2ce98a885d9f336d14
John Preston <johnprestonmail@gmail.com>
2019-09-25 17:00:05 +0300

    Support layout switching on macOS.

--- base/platform/mac/base_layout_switch_mac.mm
+++ base/platform/mac/base_layout_switch_mac.mm
@@ -6,10 +6,61 @@
 //
 #include "base/platform/mac/base_layout_switch_mac.h"
 
+#include <Carbon/Carbon.h>
+#include <Foundation/Foundation.h>
+
 namespace base::Platform {
 
 bool SwitchKeyboardLayoutToEnglish() {
-	return false;
+	auto result = false;
+
+	@autoreleasepool {
+
+	const auto kEnglish = base::flat_map<NSString*, int>{
+		{ @"com.apple.keylayout.USExtended", 0 },
+		{ @"com.apple.keylayout.ABC", 0 },
+		{ @"com.apple.keylayout.Australian", 1 },
+		{ @"com.apple.keylayout.British", 2 },
+		{ @"com.apple.keylayout.British-PC", 3 },
+		{ @"com.apple.keylayout.Canadian", 1 },
+		{ @"com.apple.keylayout.Colemak", 1 },
+		{ @"com.apple.keylayout.Dvorak", 0 },
+		{ @"com.apple.keylayout.Dvorak-Left", 0 },
+		{ @"com.apple.keylayout.DVORAK-QWERTYCMD", 0 },
+		{ @"com.apple.keylayout.Dvorak-Right", 0 },
+		{ @"com.apple.keylayout.Irish", 1 },
+		{ @"com.apple.keylayout.USInternational-PC", 4 },
+		{ @"com.apple.keylayout.US", 5 },
+	};
+
+	auto selectedLayout = (NSObject*)NULL;
+	auto selectedLevel = 0;
+	const auto offer = [&](NSObject *layout, int level) {
+		if (level > selectedLevel) {
+			selectedLayout = layout;
+			selectedLevel = level;
+		}
+	};
+
+	NSArray *list = [(NSArray *)TISCreateInputSourceList(NULL,NO) autorelease];
+	for (NSObject *layout in list) {
+		NSString *layoutId = (NSString*)TISGetInputSourceProperty(
+			(TISInputSourceRef)layout,
+			kTISPropertyInputSourceID);
+		for (const auto &[checkId, checkLevel] : kEnglish) {
+			if ([layoutId isEqualToString:checkId]) {
+				offer(layout, checkLevel);
+			}
+		}
+	}
+
+	if (selectedLayout != nullptr) {
+		TISSelectInputSource(TISInputSourceRef(selectedLayout));
+		result = true;
+	}
+
+	}
+	return result;
 }
 
 } // namespace base::Platform
