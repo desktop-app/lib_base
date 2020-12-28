86eab11dcf33f01723820f899eb495e0a3bc973c
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-12-28 07:42:58 +0400

    Looks like IsLinux32Bit/IsLinux64Bit should belong only to x86 processors

--- base/platform/linux/base_info_linux.h
+++ base/platform/linux/base_info_linux.h
@@ -7,7 +7,6 @@
 #pragma once
 
 #include "base/platform/base_platform_info.h"
-#include "base/build_config.h"
 
 namespace Platform {
 
@@ -16,19 +15,19 @@ inline constexpr bool IsLinux() {
 }
 
 inline constexpr bool IsLinux32Bit() {
-#ifdef ARCH_CPU_32_BITS
+#ifdef Q_PROCESSOR_X86_32
 	return true;
-#else // ARCH_CPU_32_BITS
+#else // Q_PROCESSOR_X86_32
 	return false;
-#endif // ARCH_CPU_32_BITS
+#endif // Q_PROCESSOR_X86_32
 }
 
 inline constexpr bool IsLinux64Bit() {
-#ifdef ARCH_CPU_64_BITS
+#ifdef Q_PROCESSOR_X86_64
 	return true;
-#else // ARCH_CPU_64_BITS
+#else // Q_PROCESSOR_X86_64
 	return false;
-#endif // ARCH_CPU_64_BITS
+#endif // Q_PROCESSOR_X86_64
 }
 
 inline constexpr bool IsWindows() { return false; }
