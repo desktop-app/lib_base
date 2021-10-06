eadd6081a7b3fb1ca13f11a6ca7f4750ecfcd64f
John Preston <johnprestonmail@gmail.com>
2021-10-06 10:49:10 +0400

    Don't use resolved WRL methods directly.

--- base/platform/win/base_windows_wrl.cpp
+++ base/platform/win/base_windows_wrl.cpp
@@ -16,34 +16,113 @@ namespace {
 
 using namespace ::Platform;
 
+HRESULT(__stdcall *RoGetActivationFactory)(
+	_In_ HSTRING activatableClassId,
+	_In_ REFIID iid,
+	_COM_Outptr_ void ** factory);
+
+HRESULT(__stdcall *RoActivateInstance)(
+	_In_ HSTRING activatableClassId,
+	_COM_Outptr_ IInspectable** instance);
+
+HRESULT(__stdcall *RoRegisterActivationFactories)(
+	_In_reads_(count) HSTRING* activatableClassIds,
+	_In_reads_(count) PFNGETACTIVATIONFACTORY* activationFactoryCallbacks,
+	_In_ UINT32 count,
+	_Out_ RO_REGISTRATION_COOKIE* cookie);
+
+void(__stdcall *RoRevokeActivationFactories)(
+	_In_ RO_REGISTRATION_COOKIE cookie);
+
+HRESULT(__stdcall *WindowsCreateString)(
+	_In_reads_opt_(length) PCNZWCH sourceString,
+	UINT32 length,
+	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING* string);
+
+HRESULT(__stdcall *WindowsCreateStringReference)(
+	_In_reads_opt_(length + 1) PCWSTR sourceString,
+	UINT32 length,
+	_Out_ HSTRING_HEADER * hstringHeader,
+	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING *string);
+
+HRESULT(__stdcall *WindowsDeleteString)(
+	_In_opt_ HSTRING string);
+
+PCWSTR(__stdcall *WindowsGetStringRawBuffer)(
+	_In_opt_ HSTRING string,
+	_Out_opt_ UINT32* length);
+
+BOOL(__stdcall *WindowsIsStringEmpty)(
+	_In_opt_ HSTRING string);
+
+HRESULT(__stdcall *WindowsStringHasEmbeddedNull)(
+	_In_opt_ HSTRING string,
+	_Out_ BOOL* hasEmbedNull);
+
+BOOL(__stdcall *RoOriginateErrorW)(
+	_In_ HRESULT error,
+	_In_ UINT cchMax,
+	_When_(cchMax == 0, _In_reads_or_z_opt_(MAX_ERROR_MESSAGE_CHARS))
+	_When_(cchMax > 0 && cchMax < MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(cchMax))
+	_When_(cchMax >= MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(MAX_ERROR_MESSAGE_CHARS)) PCWSTR message);
+
+BOOL(__stdcall *RoOriginateError)(
+	_In_ HRESULT error,
+	_In_opt_ HSTRING message);
+
 } // namespace
 
-void InitWRL() {
-	static bool Inited = false;
-	if (Inited) {
-		return;
+bool SupportsWRL() {
+	struct State {
+		bool inited = false;
+		bool supported = false;
+	};
+	static auto state = State();
+	if (state.inited) {
+		return state.supported;
 	}
-	Inited = true;
-
-	if (!IsWindows8OrGreater()) {
-		return;
+	state.inited = true;
+	if (IsWindows8OrGreater()) {
+		const auto combase = SafeLoadLibrary(L"combase.dll");
+		state.supported = combase
+			&& LOAD_SYMBOL(combase, RoGetActivationFactory)
+			&& LOAD_SYMBOL(combase, RoActivateInstance)
+			&& LOAD_SYMBOL(combase, RoRegisterActivationFactories)
+			&& LOAD_SYMBOL(combase, RoRevokeActivationFactories)
+			&& LOAD_SYMBOL(combase, WindowsCreateString)
+			&& LOAD_SYMBOL(combase, WindowsCreateStringReference)
+			&& LOAD_SYMBOL(combase, WindowsDeleteString)
+			&& LOAD_SYMBOL(combase, WindowsGetStringRawBuffer)
+			&& LOAD_SYMBOL(combase, WindowsIsStringEmpty)
+			&& LOAD_SYMBOL(combase, WindowsStringHasEmbeddedNull);
 	}
-	const auto combase = SafeLoadLibrary(L"combase.dll");
-	LOAD_SYMBOL(combase, RoGetActivationFactory);
-	LOAD_SYMBOL(combase, RoActivateInstance);
-	LOAD_SYMBOL(combase, RoRegisterActivationFactories);
-	LOAD_SYMBOL(combase, RoRevokeActivationFactories);
-	LOAD_SYMBOL(combase, WindowsCreateString);
-	LOAD_SYMBOL(combase, WindowsCreateStringReference);
-	LOAD_SYMBOL(combase, WindowsDeleteString);
-	LOAD_SYMBOL(combase, WindowsGetStringRawBuffer);
-	LOAD_SYMBOL(combase, WindowsIsStringEmpty);
-	LOAD_SYMBOL(combase, WindowsStringHasEmbeddedNull);
+	return state.supported;
 }
 
 } // namespace base::Platform
 
 namespace P = base::Platform;
+
+extern "C" {
+
+_Check_return_ HRESULT WINAPI RoActivateInstance(
+	_In_ HSTRING activatableClassId,
+	_COM_Outptr_ IInspectable** instance
+) {
+	return P::SupportsWRL()
+		? P::RoActivateInstance(activatableClassId, instance)
+		: CO_E_DLLNOTFOUND;
+}
+
+_Check_return_ HRESULT WINAPI RoGetActivationFactory(
+	_In_ HSTRING activatableClassId,
+	_In_ REFIID iid,
+	_COM_Outptr_ void** factory
+) {
+	return P::SupportsWRL()
+		? P::RoGetActivationFactory(activatableClassId, iid, factory)
+		: CO_E_DLLNOTFOUND;
+}
 
 _Check_return_ HRESULT WINAPI RoRegisterActivationFactories(
 	_In_reads_(count) HSTRING* activatableClassIds,
@@ -51,8 +130,7 @@ _Check_return_ HRESULT WINAPI RoRegisterActivationFactories(
 	_In_ UINT32 count,
 	_Out_ RO_REGISTRATION_COOKIE* cookie
 ) {
-	P::InitWRL();
-	return P::RoRegisterActivationFactories
+	return P::SupportsWRL()
 		? P::RoRegisterActivationFactories(
 			activatableClassIds,
 			activationFactoryCallbacks,
@@ -66,13 +144,10 @@ WINAPI
 RoRevokeActivationFactories(
 	_In_ RO_REGISTRATION_COOKIE cookie
 ) {
-	P::InitWRL();
-	if (P::RoRevokeActivationFactories) {
+	if (P::SupportsWRL()) {
 		P::RoRevokeActivationFactories(cookie);
 	}
 }
-
-extern "C" {
 
 STDAPI
 WindowsCreateString(
@@ -80,8 +155,7 @@ WindowsCreateString(
 	UINT32 length,
 	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING* string
 ) {
-	P::InitWRL();
-	return P::WindowsCreateString
+	return P::SupportsWRL()
 		? P::WindowsCreateString(sourceString, length, string)
 		: CO_E_DLLNOTFOUND;
 }
@@ -93,8 +167,7 @@ WindowsCreateStringReference(
 	_Out_ HSTRING_HEADER* hstringHeader,
 	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING* string
 ) {
-	P::InitWRL();
-	return P::WindowsCreateStringReference
+	return P::SupportsWRL()
 		? P::WindowsCreateStringReference(
 			sourceString,
 			length,
@@ -107,8 +180,7 @@ STDAPI
 WindowsDeleteString(
 	_In_opt_ HSTRING string
 ) {
-	P::InitWRL();
-	return P::WindowsDeleteString
+	return P::SupportsWRL()
 		? P::WindowsDeleteString(string)
 		: CO_E_DLLNOTFOUND;
 }
@@ -117,8 +189,7 @@ STDAPI_(PCWSTR)
 WindowsGetStringRawBuffer(
 		_In_opt_ HSTRING string,
 		_Out_opt_ UINT32* length) {
-	P::InitWRL();
-	return P::WindowsGetStringRawBuffer
+	return P::SupportsWRL()
 		? P::WindowsGetStringRawBuffer(string, length)
 		: nullptr;
 }
@@ -127,8 +198,7 @@ STDAPI_(BOOL)
 WindowsIsStringEmpty(
 	_In_opt_ HSTRING string
 ) {
-	P::InitWRL();
-	return P::WindowsIsStringEmpty && P::WindowsIsStringEmpty(string);
+	return P::SupportsWRL() && P::WindowsIsStringEmpty(string);
 }
 
 STDAPI
@@ -136,8 +206,7 @@ WindowsStringHasEmbeddedNull(
 	_In_opt_ HSTRING string,
 	_Out_ BOOL* hasEmbedNull
 ) {
-	P::InitWRL();
-	return P::WindowsStringHasEmbeddedNull
+	return P::SupportsWRL()
 		? P::WindowsStringHasEmbeddedNull(string, hasEmbedNull)
 		: CO_E_DLLNOTFOUND;
 }
@@ -150,8 +219,7 @@ RoOriginateErrorW(
 	_When_(cchMax > 0 && cchMax < MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(cchMax))
 	_When_(cchMax >= MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(MAX_ERROR_MESSAGE_CHARS)) PCWSTR message
 ) {
-	P::InitWRL();
-	return P::RoOriginateErrorW
+	return P::SupportsWRL()
 		&& P::RoOriginateErrorW(error, cchMax, message);
 }
 
@@ -160,8 +228,7 @@ RoOriginateError(
 	_In_ HRESULT error,
 	_In_opt_ HSTRING message
 ) {
-	P::InitWRL();
-	return P::RoOriginateError && P::RoOriginateError(error, message);
+	return P::SupportsWRL() && P::RoOriginateError(error, message);
 }
 
 } // extern "C"
--- base/platform/win/base_windows_wrl.h
+++ base/platform/win/base_windows_wrl.h
@@ -13,128 +13,15 @@
 #undef _ROAPI_
 
 #include <wrl/client.h>
+#include <winstring.h>
 #include <strsafe.h>
 #include <intsafe.h>
 
 namespace base::Platform {
 
-void InitWRL();
+[[nodiscard]] bool SupportsWRL();
 
-inline HRESULT(__stdcall *RoGetActivationFactory)(
-	_In_ HSTRING activatableClassId,
-	_In_ REFIID iid,
-	_COM_Outptr_ void ** factory);
-
-inline HRESULT(__stdcall *RoActivateInstance)(
-	_In_ HSTRING activatableClassId,
-	_COM_Outptr_ IInspectable** instance);
-
-inline HRESULT(__stdcall *RoRegisterActivationFactories)(
-	_In_reads_(count) HSTRING* activatableClassIds,
-	_In_reads_(count) PFNGETACTIVATIONFACTORY* activationFactoryCallbacks,
-	_In_ UINT32 count,
-	_Out_ RO_REGISTRATION_COOKIE* cookie);
-
-inline void(__stdcall *RoRevokeActivationFactories)(
-	_In_ RO_REGISTRATION_COOKIE cookie);
-
-inline HRESULT(__stdcall *WindowsCreateString)(
-	_In_reads_opt_(length) PCNZWCH sourceString,
-	UINT32 length,
-	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING* string);
-
-inline HRESULT(__stdcall *WindowsCreateStringReference)(
-	_In_reads_opt_(length + 1) PCWSTR sourceString,
-	UINT32 length,
-	_Out_ HSTRING_HEADER * hstringHeader,
-	_Outptr_result_maybenull_ _Result_nullonfailure_ HSTRING *string);
-
-inline HRESULT(__stdcall *WindowsDeleteString)(
-	_In_opt_ HSTRING string);
-
-inline PCWSTR(__stdcall *WindowsGetStringRawBuffer)(
-	_In_opt_ HSTRING string,
-	_Out_opt_ UINT32* length);
-
-inline BOOL(__stdcall *WindowsIsStringEmpty)(
-	_In_opt_ HSTRING string);
-
-inline HRESULT(__stdcall *WindowsStringHasEmbeddedNull)(
-	_In_opt_ HSTRING string,
-	_Out_ BOOL* hasEmbedNull);
-
-inline BOOL(__stdcall *RoOriginateErrorW)(
-	_In_ HRESULT error,
-	_In_ UINT cchMax,
-	_When_(cchMax == 0, _In_reads_or_z_opt_(MAX_ERROR_MESSAGE_CHARS))
-	_When_(cchMax > 0 && cchMax < MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(cchMax))
-	_When_(cchMax >= MAX_ERROR_MESSAGE_CHARS, _In_reads_or_z_(MAX_ERROR_MESSAGE_CHARS)) PCWSTR message);
-
-inline BOOL(__stdcall *RoOriginateError)(
-	_In_ HRESULT error,
-	_In_opt_ HSTRING message);
-
-[[nodiscard]] inline bool SupportsWRL() {
-	InitWRL();
-	return (RoGetActivationFactory != nullptr)
-		&& (WindowsCreateString != nullptr)
-		&& (WindowsCreateStringReference != nullptr)
-		&& (WindowsDeleteString != nullptr);
-}
-
-namespace details {
-
-template <class T>
-_Check_return_ __inline HRESULT GetActivationFactory(
-		_In_ HSTRING activatableClassId,
-		_COM_Outptr_ T** factory) {
-	return RoGetActivationFactory
-		? RoGetActivationFactory(activatableClassId, IID_INS_ARGS(factory))
-		: E_FAIL;
-}
-
-template <class T>
-_Check_return_ __inline  HRESULT ActivateInstance(
-		_In_ HSTRING activatableClassId,
-		_COM_Outptr_ T** instance) {
-	*instance = nullptr;
-
-	auto pInspectable = (IInspectable*)nullptr;
-	auto hr = RoActivateInstance
-		? RoActivateInstance(activatableClassId, &pInspectable)
-		: E_FAIL;
-	if (SUCCEEDED(hr)) {
-		if (__uuidof(T) == __uuidof(IInspectable)) {
-			*instance = static_cast<T*>(pInspectable);
-		} else {
-			hr = pInspectable->QueryInterface(IID_INS_ARGS(instance));
-			pInspectable->Release();
-		}
-	}
-	return hr;
-}
-
-} // namespace details
-
-template <typename T>
-inline HRESULT GetActivationFactory(
-		_In_ HSTRING activatableClassId,
-		_Inout_ Microsoft::WRL::Details::ComPtrRef<T> factory) throw() {
-	return details::GetActivationFactory(
-		activatableClassId,
-		factory.ReleaseAndGetAddressOf());
-}
-
-template <typename T>
-inline HRESULT ActivateInstance(
-		_In_ HSTRING activatableClassId,
-		_Inout_::Microsoft::WRL::Details::ComPtrRef<T> instance) throw() {
-	return details::ActivateInstance(
-		activatableClassId,
-		instance.ReleaseAndGetAddressOf());
-}
-
-class StringReferenceWrapper {
+class StringReferenceWrapper final {
 public:
 	StringReferenceWrapper(_In_reads_(length) PCWSTR stringRef, _In_ UINT32 length) throw() {
 		HRESULT hr = WindowsCreateStringReference(stringRef, length, &_header, &_hstring);
