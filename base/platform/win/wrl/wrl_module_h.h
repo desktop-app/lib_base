e4b41e9409def2f65a021571e67e8ec3ec34f90e
John Preston <johnprestonmail@gmail.com>
2021-10-06 11:02:39 +0400

    Add helper header for wrl/module.h

--- base/platform/win/wrl/wrl_module_h.h
+++ base/platform/win/wrl/wrl_module_h.h
@@ -12,5 +12,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #pragma warning(push)
 // class has virtual functions, but destructor is not virtual
 #pragma warning(disable:4265)
-#include <wrl/implements.h>
+#pragma warning(disable:5104)
+#include <wrl/module.h>
 #pragma warning(pop)
