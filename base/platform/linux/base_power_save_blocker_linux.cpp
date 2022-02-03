f93e74e1c5bcc12b156448254c623855fe171d97
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-02-03 12:36:46 +0400

    Implement display sleep prevention on X11

--- base/platform/linux/base_power_save_blocker_linux.cpp
+++ base/platform/linux/base_power_save_blocker_linux.cpp
@@ -6,15 +6,72 @@
 //
 #include "base/platform/linux/base_power_save_blocker_linux.h"
 
+#include "base/platform/base_platform_info.h"
+#include "base/timer_rpl.h"
+
+#ifndef DESKTOP_APP_DISABLE_X11_INTEGRATION
+#include "base/platform/linux/base_linux_xcb_utilities.h"
+#endif // !DESKTOP_APP_DISABLE_X11_INTEGRATION
+
 namespace base::Platform {
+namespace {
+
+#ifndef DESKTOP_APP_DISABLE_X11_INTEGRATION
+constexpr auto kResetScreenSaverTimeout = 10 * crl::time(1000);
+
+// Due to https://gitlab.freedesktop.org/xorg/xserver/-/issues/363 use the basic reset API
+void XCBPreventDisplaySleep(bool prevent) {
+	static rpl::lifetime lifetime;
+	if (!prevent) {
+		lifetime.destroy();
+		return;
+	} else if (lifetime) {
+		return;
+	}
+
+	base::timer_each(
+		kResetScreenSaverTimeout
+	) | rpl::start_with_next([] {
+		const auto connection = XCB::GetConnectionFromQt();
+		if (!connection) {
+			return;
+		}
+		xcb_force_screen_saver(connection, XCB_SCREEN_SAVER_RESET);
+	}, lifetime);
+}
+#endif // !DESKTOP_APP_DISABLE_X11_INTEGRATION
+
+} // namespace
 
 void BlockPowerSave(
 	PowerSaveBlockType type,
 	const QString &description,
 	QPointer<QWindow> window) {
+	switch (type) {
+	case PowerSaveBlockType::PreventAppSuspension:
+		break;
+	case PowerSaveBlockType::PreventDisplaySleep:
+#ifndef DESKTOP_APP_DISABLE_X11_INTEGRATION
+		if (::Platform::IsX11()) {
+			XCBPreventDisplaySleep(true);
+		}
+#endif // !DESKTOP_APP_DISABLE_X11_INTEGRATION
+		break;
+	}
 }
 
 void UnblockPowerSave(PowerSaveBlockType type, QPointer<QWindow> window) {
+	switch (type) {
+	case PowerSaveBlockType::PreventAppSuspension:
+		break;
+	case PowerSaveBlockType::PreventDisplaySleep:
+#ifndef DESKTOP_APP_DISABLE_X11_INTEGRATION
+		if (::Platform::IsX11()) {
+			XCBPreventDisplaySleep(false);
+		}
+#endif // !DESKTOP_APP_DISABLE_X11_INTEGRATION
+		break;
+	}
 }
 
 } // namespace base::Platform
