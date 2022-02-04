462a1925eb5df46a6406773df48a04ebe538446a
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-02-04 06:37:17 +0400

    Implement display sleep prevention on Wayland

---
+++ base/platform/linux/base_linux_wayland_integration.h
@@ -0,0 +1,30 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+class QWindow;
+
+namespace base {
+namespace Platform {
+
+class WaylandIntegration {
+public:
+	[[nodiscard]] static WaylandIntegration *Instance();
+
+	void preventDisplaySleep(bool prevent, QWindow *window);
+
+private:
+	WaylandIntegration();
+	~WaylandIntegration();
+
+	struct Private;
+	const std::unique_ptr<Private> _private;
+};
+
+} // namespace Platform
+} // namespace base
