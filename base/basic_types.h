ef33591c1b8d8cf356f760768dc32e7cae82bf1d
John Preston <johnprestonmail@gmail.com>
2022-10-05 12:08:52 +0400

    Provide non-allocating "..."_q literals.

--- base/basic_types.h
+++ base/basic_types.h
@@ -50,6 +50,105 @@ using float64 = double;
 
 using TimeId = int32;
 
+#ifdef _MSC_VER
+#define DESKTOP_APP_USE_NO_ALLOCATION_LITERAL
+#endif // _MSC_VER
+
+#ifdef DESKTOP_APP_USE_NO_ALLOCATION_LITERAL
+
+namespace base::details {
+
+template <size_t N>
+struct LiteralResolver {
+	template <size_t ...I>
+	constexpr LiteralResolver(
+		const char16_t (&text)[N],
+		std::index_sequence<I...>)
+	: utf16text{ text[I]... } {
+	}
+	template <size_t ...I>
+	constexpr LiteralResolver(
+		const char (&text)[N],
+		std::index_sequence<I...>)
+	: utf8text{ text[I]... }
+	, utf8(true) {
+	}
+	constexpr LiteralResolver(const char16_t (&text)[N])
+	: LiteralResolver(text, std::make_index_sequence<N>{}) {
+	}
+	constexpr LiteralResolver(const char (&text)[N])
+	: LiteralResolver(text, std::make_index_sequence<N>{}) {
+	}
+
+	constexpr auto operator<=>(const LiteralResolver &) const = default;
+
+	const char16_t utf16text[N]{};
+	const char utf8text[N]{};
+	size_t length = N;
+	bool utf8 = false;
+};
+
+template <size_t N>
+struct StaticStringData {
+	template <std::size_t... I>
+	constexpr StaticStringData(
+		const char16_t (&text)[N],
+		std::index_sequence<I...>)
+	: data Q_STATIC_STRING_DATA_HEADER_INITIALIZER(N - 1)
+	, text{ text[I]... } {
+	}
+	QArrayData data;
+	char16_t text[N];
+
+	QStringData *pointer() {
+		Q_ASSERT(data.ref.isStatic());
+		return static_cast<QStringData*>(&data);
+	}
+};
+
+template <size_t N>
+struct StaticByteArrayData {
+	template <std::size_t... I>
+	constexpr StaticByteArrayData(
+		const char (&text)[N],
+		std::index_sequence<I...>)
+	: data Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER(N - 1)
+	, text{ text[I]... } {
+	}
+	QByteArrayData data;
+	char text[N];
+
+	QByteArrayData *pointer() {
+		Q_ASSERT(data.ref.isStatic());
+		return &data;
+	}
+};
+
+template <LiteralResolver Resolve>
+using q_literal_type = std::conditional_t<Resolve.utf8, QByteArray, QString>;
+
+} // namespace base::details
+
+template <base::details::LiteralResolver Resolve>
+base::details::q_literal_type<Resolve> operator""_q() {
+	static_assert(Resolve.length > 0);
+
+	using namespace base::details;
+	if constexpr (Resolve.utf8) {
+		static auto Literal = StaticByteArrayData<Resolve.length>(
+			Resolve.utf8text,
+			std::make_index_sequence<Resolve.length>{});
+		return QByteArray{ QByteArrayDataPtr{ Literal.pointer() } };
+	} else {
+		static auto Literal = StaticStringData<Resolve.length>(
+			Resolve.utf16text,
+			std::make_index_sequence<Resolve.length>{});
+		return QString{ QStringDataPtr{ Literal.pointer() } };
+	}
+};
+
+#else // DESKTOP_APP_USE_NO_ALLOCATION_LITERAL
+
 [[nodiscard]] inline QByteArray operator""_q(
 		const char *data,
 		std::size_t size) {
@@ -63,3 +162,5 @@ using TimeId = int32;
 		reinterpret_cast<const QChar*>(data),
 		size);
 }
+
+#endif // DESKTOP_APP_USE_NO_ALLOCATION_LITERAL
