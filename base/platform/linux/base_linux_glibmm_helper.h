7b2b813b7d412e27ebd468fdc085e2ee8e6fccf1
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-11-15 01:46:23 +0400

    Add Glib::Variant compatibility with qint64/quint64

--- base/platform/linux/base_linux_glibmm_helper.h
+++ base/platform/linux/base_linux_glibmm_helper.h
@@ -6,7 +6,103 @@
 //
 #pragma once
 
+#include "base/basic_types.h"
+
 #include <glibmm/variant.h>
+
+namespace Glib {
+
+template <>
+class Variant<int64> : public VariantBase
+{
+public:
+	using CType = gint64;
+
+	/// Default constructor.
+	Variant() : VariantBase() {
+	}
+
+	/** GVariant constructor.
+	* @param castitem The GVariant to wrap.
+	* @param take_a_reference Whether to take an extra reference of the
+	* GVariant or not (not taking one could destroy the GVariant with the
+	* wrapper).
+	*/
+	explicit Variant(GVariant *castitem, bool take_a_reference = false)
+	: VariantBase(castitem, take_a_reference) {
+	}
+
+	/** Gets the Glib::VariantType.
+	* @return The Glib::VariantType.
+	*/
+	static const VariantType &variant_type() G_GNUC_CONST {
+		static VariantType type(G_VARIANT_TYPE_INT64);
+		return type;
+	}
+
+	/** Creates a new Glib::Variant<int64>.
+	* @param data The value of the new Glib::Variant<int64>.
+	* @return The new Glib::Variant<int64>.
+	*/
+	static Variant<int64> create(int64 data) {
+		auto result = Variant<int64>(g_variant_new_int64(data));
+		return result;
+	}
+
+	/** Gets the value of the Glib::Variant<int64>.
+	* @return The int64 value of the Glib::Variant<int64>.
+	*/
+	int64 get() const {
+		return g_variant_get_int64(gobject_);
+	}
+};
+
+template <>
+class Variant<uint64> : public VariantBase
+{
+public:
+	using CType = guint64;
+
+	/// Default constructor.
+	Variant() : VariantBase() {
+	}
+
+	/** GVariant constructor.
+	* @param castitem The GVariant to wrap.
+	* @param take_a_reference Whether to take an extra reference of the
+	* GVariant or not (not taking one could destroy the GVariant with the
+	* wrapper).
+	*/
+	explicit Variant(GVariant *castitem, bool take_a_reference = false)
+	: VariantBase(castitem, take_a_reference) {
+	}
+
+	/** Gets the Glib::VariantType.
+	* @return The Glib::VariantType.
+	*/
+	static const VariantType &variant_type() G_GNUC_CONST {
+		static VariantType type(G_VARIANT_TYPE_UINT64);
+		return type;
+	}
+
+	/** Creates a new Glib::Variant<uint64>.
+	* @param data The value of the new Glib::Variant<uint64>.
+	* @return The new Glib::Variant<uint64>.
+	*/
+	static Variant<uint64> create(uint64 data) {
+		auto result = Variant<uint64>(g_variant_new_uint64(data));
+		return result;
+	}
+
+	/** Gets the value of the Glib::Variant<uint64>.
+	* @return The uint64 value of the Glib::Variant<uint64>.
+	*/
+	uint64 get() const {
+		return g_variant_get_uint64(gobject_);
+	}
+};
+
+} // namespace Glib
 
 namespace base {
 namespace Platform {
