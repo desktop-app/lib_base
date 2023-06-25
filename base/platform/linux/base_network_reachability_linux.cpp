747c4aec6e42a9161269a443fd624ac7b46371ff
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-06-25 11:16:12 +0400

    Use QNetworkInformation on Linux
    We now link QGlibNetworkInformationPlugin

--- base/platform/linux/base_network_reachability_linux.cpp
+++ base/platform/linux/base_network_reachability_linux.cpp
@@ -6,49 +6,10 @@
 //
 #include "base/platform/base_platform_network_reachability.h"
 
-#include <giomm.h>
-
 namespace base::Platform {
-namespace {
 
-class NetworkReachabilityImpl
-	: public NetworkReachability
-	, public sigc::trackable {
-public:
-	NetworkReachabilityImpl()
-	: _monitor(Gio::NetworkMonitor::get_default())
-	, _available(_monitor->get_network_available()) {
-		_monitor->property_network_available(
-		).signal_changed(
-		).connect(sigc::track_object([=] {
-			_available = _monitor->get_network_available();
-		}, *this));
-	}
-
-	rpl::producer<bool> availableValue() const override {
-		return _available.value();
-	}
-
-private:
-	Glib::RefPtr<Gio::NetworkMonitor> _monitor;
-	rpl::variable<bool> _available;
-};
-
-} // namespace
-
-// glib is better on linux due to portal support
 std::unique_ptr<NetworkReachability> NetworkReachability::Create() {
-	// crashes on 2.46.0...2.60.0, but not on >=2.56.3, >=2.58.1 (fix backported)
-	if ((!glib_check_version(2, 56, 0) && glib_check_version(2, 56, 3))
-		|| (!glib_check_version(2, 58, 0) && glib_check_version(2, 58, 1))
-		|| (glib_check_version(2, 60, 0)
-			&& glib_check_version(2, 58, 0)
-			&& glib_check_version(2, 56, 0)
-			&& !glib_check_version(2, 46, 0))) {
-		return nullptr;
-	}
-
-	return std::make_unique<NetworkReachabilityImpl>();
+	return nullptr;
 }
 
 } // namespace base::Platform
