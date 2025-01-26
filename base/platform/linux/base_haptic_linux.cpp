85dda6c3c8f7f897aa1c9824e0a9da35a42a38de
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-01-26 15:50:36 +0400

    Implement base::Platform::Haptic on Linux

--- base/platform/linux/base_haptic_linux.cpp
+++ base/platform/linux/base_haptic_linux.cpp
@@ -6,9 +6,44 @@
 //
 #include "base/platform/linux/base_haptic_linux.h"
 
+#include <QtGui/QGuiApplication>
+
+#include <sigxcpufeedback/sigxcpufeedback.hpp>
+
 namespace base::Platform {
+namespace {
+
+using namespace gi::repository;
+namespace GObject = gi::repository::GObject;
+
+} // namespace
 
 void Haptic() {
+	SigxcpuFeedback::HapticProxy::new_for_bus(
+		Gio::BusType::SESSION_,
+		Gio::DBusProxyFlags::NONE_,
+		"org.sigxcpu.Feedback",
+		"/org/sigxcpu/Feedback",
+		[=](GObject::Object, Gio::AsyncResult res) {
+			auto interface = SigxcpuFeedback::Haptic(
+				SigxcpuFeedback::HapticProxy::new_for_bus_finish(
+					res,
+					nullptr));
+
+			if (!interface) {
+				return;
+			}
+
+			interface.call_vibrate(
+				QGuiApplication::desktopFileName().toStdString(),
+				GLib::Variant::new_array({
+					GLib::Variant::new_tuple({
+						GLib::Variant::new_double(0.2),
+						GLib::Variant::new_uint32(5),
+					}),
+				}),
+				nullptr);
+		});
 }
 
 } // namespace base::Platform
