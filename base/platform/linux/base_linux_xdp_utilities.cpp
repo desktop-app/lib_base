720eaf44a529da50f5277fd6874318d9a08b735a
John Preston <johnprestonmail@gmail.com>
2025-04-04 14:22:07 +0400

    Apply suggestions from code review.
    Co-authored-by: ilya-fedin <fedin-ilja2010@ya.ru>

--- base/platform/linux/base_linux_xdp_utilities.cpp
+++ base/platform/linux/base_linux_xdp_utilities.cpp
@@ -18,9 +18,9 @@
 #include <private/qguiapplication_p.h>
 #if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
 #include <private/qdesktopunixservices_p.h>
-#else
+#else // Qt >= 6.9.0
 #include <private/qgenericunixservices_p.h>
-#endif // Qt >= 6.9.0
+#endif // Qt < 6.9.0
 #endif // Qt >= 6.5.0
 
 #include <sstream>
@@ -43,11 +43,10 @@ std::string ParentWindowID(QWindow *window) {
 	}
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
-#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+#if QT_VERSION < QT_VERSION_CHECK(6, 9, 0)
+	using QDesktopUnixServices = QGenericUnixServices;
+#endif // Qt < 6.9.0
 	if (const auto services = dynamic_cast<QDesktopUnixServices*>(
-#else
-	if (const auto services = dynamic_cast<QGenericUnixServices*>(
-#endif // Qt >= 6.9.0
 			QGuiApplicationPrivate::platformIntegration()->services())) {
 		return services->portalWindowIdentifier(window).toStdString();
 	}
