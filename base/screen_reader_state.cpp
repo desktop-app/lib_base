90fa46f77b4d8c77ae0924f05b505e57742cdc63
Reza Bakhshi Laktasaraei <74649066+rezabakhshilaktasaraei@users.noreply.github.com>
2025-09-19 11:58:22 +0330

    Introducing detect screen reader state by API

---
+++ base/screen_reader_state.cpp
@@ -0,0 +1,26 @@
+#include "base/screen_reader_state.h"
+
+namespace base {
+	ScreenReaderState* ScreenReaderState::Instance() {
+		static auto instance = ScreenReaderState();
+		return &instance;
+	}
+
+ScreenReaderState::ScreenReaderState()
+	: _isActive(QAccessible::isActive()) {
+	QAccessible::installActivationObserver(this);
+}
+
+	ScreenReaderState::~ScreenReaderState() {
+		QAccessible::removeActivationObserver(this);
+	}
+
+	void ScreenReaderState::accessibilityActiveChanged(bool active) {
+		_isActive = active;
+	}
+
+	rpl::producer<bool> ScreenReaderState::activeValue() const {
+		return _isActive.value();
+	}
+
+} // namespace base
