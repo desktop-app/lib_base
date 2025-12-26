desktop-app/lib_base

commit f2be5c4ab5f9e2b5b543a68b4953cd6b643ed045

Provide full desktop environment information within DeviceModelPretty

base/platform/linux/base_info_linux.cpp
@@ -34,11 +34,8 @@ struct wl_display;
 namespace Platform {
 namespace {
 
-[[nodiscard]] QString GetDesktopEnvironment() {
-	const auto value = qEnvironmentVariable("XDG_CURRENT_DESKTOP");
-	return value.contains(':')
-		? value.left(value.indexOf(':'))
-		: value;
+[[nodiscard]] QStringList GetDesktopEnvironment() {
+	return qEnvironmentVariable("XDG_CURRENT_DESKTOP").trimmed().split(':');
 }
 
 [[nodiscard]] QString ChassisTypeToString(uint type) {

commit edebb750e6ebe7853f13e7f542199e4c7a021012

Fix desktop environment empty check
Regression was introduced in f2be5c4ab5f9e2b5b543a68b4953cd6b643ed045

base/platform/linux/base_info_linux.cpp
@@ -38,6 +38,8 @@ namespace {
 	const auto list = qEnvironmentVariable("XDG_CURRENT_DESKTOP").split(':');
 	return list | ranges::views::transform([](const auto &item) {
 		return item.simplified();
+	}) | ranges::views::filter([](const auto &item) {
+		return !item.isEmpty();
 	}) | ranges::to<QStringList>;
 }
 