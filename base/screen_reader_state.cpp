fd1a2fa140e60f6de918ef6d2100ca6c74b98fa8
Ilya Fedin <fedin-ilja2010@ya.ru>
2026-06-28 02:49:37 +0400

    Get rid of new Q_OS_LINUX

--- base/screen_reader_state.cpp
+++ base/screen_reader_state.cpp
@@ -4,9 +4,9 @@
 #include "base/platform/mac/base_screen_reader_state_mac.h"
 #elif defined(Q_OS_WIN) // Q_OS_MAC
 #include "base/platform/win/base_screen_reader_state_win.h"
-#elif defined(Q_OS_LINUX) // Q_OS_MAC || Q_OS_WIN
+#else
 #include "base/platform/linux/base_screen_reader_state_linux.h"
-#endif // Q_OS_MAC || Q_OS_WIN || Q_OS_LINUX
+#endif // Q_OS_MAC || Q_OS_WIN
 
 namespace base {
 
@@ -17,11 +17,9 @@ ScreenReaderState *ScreenReaderState::Instance() {
 	static auto instance = Platform::MacScreenReaderState();
 #elif defined(Q_OS_WIN) // Q_OS_MAC
 	static auto instance = Platform::WinScreenReaderState();
-#elif defined(Q_OS_LINUX) // Q_OS_MAC || Q_OS_WIN
+#else // Q_OS_MAC || Q_OS_WIN
 	static auto instance = Platform::LinuxScreenReaderState();
-#else // Q_OS_MAC || Q_OS_WIN || Q_OS_LINUX
-	static auto instance = GeneralScreenReaderState();
-#endif // Q_OS_MAC || Q_OS_WIN || Q_OS_LINUX
+#endif // Q_OS_MAC || Q_OS_WIN
 	return &instance;
 }
 
